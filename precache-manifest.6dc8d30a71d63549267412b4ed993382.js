475524ad246c70b09e896f320009c13c8d2b2235
Sunny Heyar <sunny.heyar@gmail.com>
2019-06-22 23:22:15 -0400

    Updates

---
+++ precache-manifest.6dc8d30a71d63549267412b4ed993382.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16dd21084b30319760985472da31a84",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9f3d3b0b879f6bf35c89",
+    "url": "/clicky-game/static/css/main.72aee1a5.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "9f3d3b0b879f6bf35c89",
+    "url": "/clicky-game/static/js/main.1d4b8399.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
